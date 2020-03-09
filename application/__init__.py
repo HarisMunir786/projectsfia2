4061fe2ee81c4fca9d31514999e456eedd18fe50
HarisMunir786 <Haris.Munir@qa.com>
2020-03-09 16:14:31 +0000

    changes to __init.py, models.py, routes. py and create.py created

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,28 @@
 # import Flask class from the flask module
 from flask import Flask
+# import SQLAlchemy class from the flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+# import Bcrypt class from the flask_bcrypt
+from flask_bcrypt import Bcrypt
+# import LoginManager class from flask_login
+from flask_login import LoginManager
+from os import getenv
+import os
+
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
+# create a new instance of bcrypt and store it in Bcrypt(app)
+bcrypt = Bcrypt(app)
+
+# create a new instance of login_manager and store it in LoginManager(app)
+login_manager = LoginManager(app)
+# create a new view of login_manager view
+login_manager.login_view = 'login'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('MY_SECRET_KEY')
+db = SQLAlchemy(app)
+
 # import the ./application/routes.py file
 from application import routes
---
+++ application/models.py
@@ -0,0 +1,19 @@
+from application import db
+from application import login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	email = db.Column(db.String(100), nullable=False)
+	password = db.Column(db.String(500), nullable=False)
+	def __repr__(self):
+		return ''.join([
+			'Name: ', self.name, '\r\n',
+			'Email: ', self.email
+			])
+
